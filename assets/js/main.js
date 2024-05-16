4d3b3cd2f8b5713cf92baa862a42cb3d72391f19
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-05-15 21:25:58 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+import Module from "./module";
+
+new Module();
+console.log("patate");
---
+++ assets/js/module.js
@@ -0,0 +1,5 @@
+export default class Module {
+    constructor() {
+        console.log("Module constructor");
+    }
+}
